fe5739a27f557e61df89dd841a39dbf7bdf3893f
sibesty <31071011+sibesty@users.noreply.github.com>
2017-08-16 16:48:45 +0100

    initial commit
    initial site files upload

---
+++ index_files/b97b686d5236bd9d5a3aae1f852c2235-4.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="generator" content="RapidWeaver" />
+		
+		<title>minor website changes | besty.co.uk   </title>
+		<link rel="stylesheet" type="text/css" media="all" href="http://www.besty.co.uk/rw_common/themes/silk/styles.css" />
+		<link rel="stylesheet" type="text/css" media="all" href="http://www.besty.co.uk/rw_common/themes/silk/color_tags.css" />
+		<!--[if lt IE 7]>
+			<script type="text/javascript">
+				if (navigator.platform == "Win32" && navigator.appName == "Microsoft Internet Explorer" && window.attachEvent) {
+					window.attachEvent("onload", enableAlphaImages);
+				}
+
+				function enableAlphaImages(){
+					var rslt = navigator.appVersion.match(/MSIE (\d+\.\d+)/, '');
+					var itsAllGood = (rslt != null && Number(rslt[1]) >= 5.5);
+					if (itsAllGood) {
+						for (var i=0; i<document.all.length; i++){
+							var obj = document.all[i];
+							var bg = obj.currentStyle.backgroundImage;
+							var img = document.images[i];
+							if (bg && bg.match(/\.png/i) != null) {
+								var img = bg.substring(5,bg.length-2);
+								var offset = obj.style["background-position"];
+								obj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+img+"', sizingMethod='crop')";
+								obj.style.backgroundImage = "url('http://www.besty.co.uk/rw_common/themes/silk/blank.gif')";
+								obj.style["background-position"] = offset; // reapply
+							} else if (img && img.src.match(/\.png$/i) != null) {
+								var src = img.src;
+								img.style.width = img.width + "px";
+								img.style.height = img.height + "px";
+								img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='crop')";
+								img.src = "http://www.besty.co.uk/rw_common/themes/silk/blank.gif";
+							}
+
+						}
+					}
+				}
+			</script>
+			<link rel="stylesheet" type="text/css" media="all" href="http://www.besty.co.uk/rw_common/themes/silk/ie.css" />
+			<style>
+				#breadcrumbs{
+					background: none;
+					filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='http://www.besty.co.uk/rw_common/themes/silk/images/breadcrumbs-background.png',sizingMethod='scale');
+				}
+				.clearfix{ display: inline-block; }
+			</style>
+		<![endif]-->
+		<!--[if IE 7]>
+			<style>
+				.clearfix{ display: inline-block; }
+				#content-footer-inside{
+					padding-top: 14px;
+				}
+			</style>
+		<![endif]-->
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/font/lucida.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/size/11.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/nav_size/11.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/width/840.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/title_slogan/title_and_slogan.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/logo_position/left.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/breadcrumbs/show.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/sidebar/right.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/banner/banner_10.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/header_highlight/hide.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="http://www.besty.co.uk/rw_common/themes/silk/css/highlight/show.css" />
+		
+		
+		<script type="text/javascript" src="http://www.besty.co.uk/rw_common/themes/silk/javascript.js"></script>
+		
+		<link rel="alternate" type="application/rss+xml" title="My RSS Feed" href="http://www.besty.co.uk/index_files/feed.xml" />
+		
+	</head>
+	<body>
+		
+		<div id="outer-wrapper">
+		<div id="wrapper">
+
+			<!-- Header -->
+			<div id="header">
+				
+				<h2><span id="title">besty.co.uk   </span> <span id="slogan">the website of SiB</span></h2>
+				<div id="toolbar"><ul><li><a href="../" rel="self" class="current"><span>home</span></a></li><li><a href="../mixes.html" rel="self"><span>mixes</span></a></li><li><a href="../about.html" rel="self"><span>about me</span></a></li></ul></div>
+			</div>
+
+			<!-- Banner -->
+			<div id="banner">
+				<div id="highlight"></div>
+				<div id="breadcrumbs"><div class="pad"><ul><li><a href="../">home</a>&nbsp;/&nbsp;</li></ul></div></div>
+			</div>
+
+			<!-- Content Wrapper -->
+			<div id="content-wrapper" class="clearfix">
+					
+				<!-- Sidebar -->
+				<div id="sidebar">
+					<div id="sidebar-toolbar"><ul><li><a href="../" rel="self" class="current"><span>home</span></a></li><li><a href="../mixes.html" rel="self"><span>mixes</span></a></li><li><a href="../about.html" rel="self"><span>about me</span></a></li></ul></div>
+					<div id="sidebar-content">
+						<h3 id="sidebar-title"></h3>
+						
+					</div>
+					<?php include("sidebar-content.html"); ?>
+				</div>
+
+				<!-- Content -->
+				<div id="content">	
+	
+	<div class="blog-archive-entries-wrapper">
+		<div id="unique-entry-id-4" class="blog-entry"><h1 class="blog-entry-title">minor website changes</h1><div class="blog-entry-date">13/Jun/2013 15:10 </div><div class="blog-entry-body">Admin: Some minor site formatting and SEO enhancements</div></div>
+	</div>
+	
+	 
+</div>
+							
+			</div>
+
+			<!-- Content Footer -->
+			<div id="content-footer"><div id="content-footer-inside"></div></div>
+			
+			<!-- Footer -->
+			<div id="footer">&copy; 2013 Besty</div>
+			
+		</div>
+		</div>
+		
+	</body>
+</html>
